ae80fc6840f01e6055de1053366672c5dff03d13
Nicholas Bishop <nickbishop97@gmail.com>
2021-12-01 17:57:18 -0500

    Add solution for day1, part 1

--- day1/part1/main.cpp
+++ day1/part1/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 
 int main() {
   std::vector<std::string> input;
-
+  
   for (std::string line; std::getline(std::cin, line);) {
     input.push_back(line);
-    std::cout << line << std::endl;
   }
+  
+  int depth_previous = std::stoi(input.at(0));
+  int depth_current = 0;
+  int count_increasing = 0;
+  
+  for (auto iter = std::next(input.begin()); iter != input.end(); ++iter) {
+    depth_current = std::stoi(*iter);
+
+    if (depth_current > depth_previous) {
+      count_increasing++;
+    }
+
+    depth_previous = depth_current;
+  }
+
+  std::cout << "The measured depth increased " << count_increasing << " times." << std::endl;
   
   return 0;
 }
