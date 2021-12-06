24b6f83fcf41cea9ed4804ac2c66e770a52f291b
Nicholas Bishop <nickbishop97@gmail.com>
2021-12-06 12:49:25 -0500

    add solution to day6, part1

---
+++ day6/part1/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+void next_cycle(std::map<int, int>& lanternfish_cycles);
+void iterate(std::map<int, int>& lanternfish_cycles, int num_cycles);
+int get_total_lanternfish(std::map<int, int>& lanternfish_cycles);
+void print_cycles(std::map<int, int>& lanternfish_cycles);
+  
+int main() {
+  std::string input;
+  std::map<int, int> lanternfish_cycles;
+
+  std::getline(std::cin, input);
+  
+  for (int i = 0; i <= 8; i++) {
+    lanternfish_cycles.insert(std::pair<int, int>(i, 0));
+  }
+
+  std::size_t pos = 0;
+  if (input.find(',', pos) != std::string::npos) {
+    lanternfish_cycles.at(std::stoi(input.substr(pos, input.find(',', pos)))) += 1;
+  }
+  else {
+    lanternfish_cycles.at(std::stoi(input)) += 1;
+  }
+  
+  while (input.find(',', pos) != std::string::npos) {
+    pos = input.find(',', pos) + 1;
+    lanternfish_cycles.at(std::stoi(input.substr(pos, input.find(',', pos)))) += 1; 
+  }
+
+  iterate(lanternfish_cycles, 80);
+
+  std::cout << "The number of lanternfish after 80 days is "
+	    << get_total_lanternfish(lanternfish_cycles) << std::endl;
+  return 0;
+}
+
+void next_cycle(std::map<int, int>& lanternfish_cycles) {
+  int count_new_lanternfish = lanternfish_cycles.at(0);
+
+  for (int i = 0; i < 8; i++) {
+    lanternfish_cycles.at(i) = lanternfish_cycles.at(i + 1);
+  }
+
+  lanternfish_cycles.at(6) += count_new_lanternfish;
+  lanternfish_cycles.at(8) = count_new_lanternfish;
+}
+
+void iterate(std::map<int, int>& lanternfish_cycles, int num_cycles) {
+  for (int i = 0; i < num_cycles; i++) {
+    next_cycle(lanternfish_cycles);
+  }
+}
+
+int get_total_lanternfish(std::map<int, int>& lanternfish_cycles) {
+  int count;
+  
+  for (auto iter = lanternfish_cycles.begin(); iter != lanternfish_cycles.end(); ++iter) {
+    count += iter->second;
+  }
+
+  return count;
+}
+
+void print_cycles(std::map<int, int>& lanternfish_cycles) {
+  for (auto iter = lanternfish_cycles.begin(); iter != lanternfish_cycles.end(); ++iter) {
+    std::cout << iter->first << " days: " << iter->second << std::endl; 
+  }
+}
